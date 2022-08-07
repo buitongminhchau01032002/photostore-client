8ca262143e395f14fe24050e088c5dfee9321b80
buitongminhchau01032002 <buitongminhchau01032002@gmail.com>
2022-08-07 14:41:09 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./*.{html,js}'],
+    theme: {
+        extend: {
+            spacing: {
+                'h-header': 'var(--h-header)',
+            },
+
+            colors: {
+                primary:
+                    'hsl(var(--clr-primary-h) var(--clr-primary-s) var(--clr-primary-l) / <alpha-value>)',
+                'primary-hover':
+                    'hsl(var(--clr-primary-h) var(--clr-primary-s) var(--clr-primary-hover-l) / <alpha-value>)',
+                'clr-text': 'hsl(var(--clr-text) / <alpha-value>)',
+                'clr-text-dark': 'hsl(var(--clr-text-dark) / <alpha-value>)',
+                'light-gray': 'hsl(var(--light-gray) / <alpha-value>)',
+                white: 'hsl(var(--clr-white) / <alpha-value>)',
+
+                transparent: 'transparent',
+                current: 'currentColor',
+            },
+
+            zIndex: {
+                header: 100,
+            },
+        },
+        screens: {
+            sm: '576px',
+            md: '960px',
+            lg: '1280px',
+            xl: '1540px',
+        },
+    },
+    plugins: [],
+};
