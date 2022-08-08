a8859eb5c19530a0221de57f968a8271fe9c441e
buitongminhchau01032002 <buitongminhchau01032002@gmail.com>
2022-08-08 19:27:35 +0700

    up

--- js/image.js
+++ js/image.js
@@ -2,7 +2,8 @@ const placeholderImages = () => {
     const imgs = document.querySelectorAll('img');
     imgs.forEach((img) => {
         img.addEventListener('error', () => {
-            img.src = '../assets/placeholder.png';
+            img.src =
+                'https://res.cloudinary.com/dbxfq9usa/image/upload/v1659961165/placeholder_qeff7x.png';
         });
     });
 };
